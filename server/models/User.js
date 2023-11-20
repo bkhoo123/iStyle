afaed0a66cda993b8c4804695d9ef0d1812dd34a
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-11-19 20:37:05 -0800

    User Model Created

---
+++ server/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: true, 
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true, 
+  },
+  sex: {
+    type: String, 
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  }
+})
+
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+
+const User = require('./models/User');
+
+app.use(cors());
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+mongoose.connect('mongodb+srv://khoobrian123:aQew4uagc2Drtx6K@istyle.il9j72h.mongodb.net/istyle?retryWrites=true&w=majority');
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+});
