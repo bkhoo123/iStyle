099eb3bde32a7acd67408623a5a43b1f2be58ae2
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2024-01-15 10:14:19 -0800

    Signup Tested with Database

---
+++ server/app/routes/userRoutes.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+router.post('/signup', async (req, res) => {
+  try {
+    // Check if the user already exists 
+    const userExists = await User.findOne({email: req.body.email})
+    if (userExists) {
+      return res.status(400).send("Email already in use")
+    }
+    console.log(userExists, 'userExists')
+
+    // Create a new user 
+    const {name, email, password, sex, height} = req.body;
+
+    const user = new User({name, email, password, sex, height})
+
+    console.log(user, "user")
+
+    // Save the user in the database
+    await user.save();
+    res.status(201).send("User created Successfully")
+  } catch (error) {
+    res.status(500).send('Error during registration: ' + error.message )
+  }
+})
+
+
+router.post('/login', async (req, res) => {
+  try {
+    // Find the user by email 
+    const user = await User.findOne({ email: req.body.email})
+    if (!user) {
+      return res.status(400).send('Invalid email or password')
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).send("Invalid email or password")
+    }
+
+    res.send("Logged in successfully")
+  } catch (error) {
+    res.status(500).send('Error during login: ' + error.message)
+  }
+})
+
+router.delete("/", async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).send("")
+  }
+})
+
+module.exports = router;
